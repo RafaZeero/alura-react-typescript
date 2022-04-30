068d354dc4300879fb6161b0a0254ddbefce732a
rafazeero <rafaa99@gmail.com>
2022-04-29 21:20:47 -0300

    Início projeto Alura React c/ Ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <h1>App Vite</h1>
+    </div>
+  )
+}
+
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Button() {
+  return (
+    <div>
+      <h1>Botao</h1>
+
+    </div>
+  )
+}
