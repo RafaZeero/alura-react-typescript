d0d1cc83d8684eebd29efe7ecdde3d24a569737b
rafazeero <rafaa99@gmail.com>
2022-05-02 13:32:27 -0300

    aula 04 inicio

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Form from "../../components/Form";
 import List from "../../components/List";
-import styles from './styles.module.scss'
+import styles from './Home.module.scss'
 
 
 export default function App() {
